a11e26895c4b23a3f36e2ea1c898c114a0d3f958
Brian Dorsey <brian@dorseys.org>
2024-01-01 19:08:25 -0800

    attempt to use enums in vec for chunks

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,22 @@ use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
 
-struct WavChunk {
+struct FileChunk {
     offset: u32,
     chunk_id: [u8; 4],
     chunk_size: u32,
+    chunk: Chunk,
 }
 
-impl Display for WavChunk {
+impl Display for FileChunk {
     fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
         write!(
             f,
-            "{:12} {:8} {:-10} ",
+            "{:12} {:8} {:-10} {}",
             self.offset,
             String::from_utf8_lossy(&self.chunk_id),
-            self.chunk_size
+            self.chunk_size,
+            self.chunk
         )
     }
 }
@@ -29,23 +31,23 @@ impl Display for WavChunk {
 // based on http://soundfile.sapp.org/doc/WaveFormat/
 #[derive(BinRead, Debug)]
 #[br(little)]
+#[allow(dead_code)]
 struct FmtChunk {
     #[br(big)]
-    chunk_id: [u8; 4],
-    chunk_size: u32,
     audio_format: u16,
     num_channels: u16,
     sample_rate: u32,
     byte_rate: u32,
     block_align: u16,
     bits_per_sample: u16,
+    // currently ignoring optional extra data
     // padding to end of chunk_size
 }
 
+#[allow(dead_code)]
 impl FmtChunk {
-    fn _chunk_id() -> [u8; 4] {
-        *b"fmt "
-    }
+    const DESC: &'static str = "desc of 'fmt ' chunk TOOD";
+    const SAMPLE_RATE_DESC: &'static str = "the sample rate TODO";
 }
 
 impl Display for FmtChunk {
@@ -61,8 +63,22 @@ impl Display for FmtChunk {
     }
 }
 
+enum Chunk {
+    UnParsed,
+    Fmt(FmtChunk),
+}
+
+impl Display for Chunk {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        let output = match self {
+            Chunk::UnParsed => "...".to_string(),
+            Chunk::Fmt(chunk) => format!("{chunk}"),
+        };
+        write!(f, "{}", output)
+    }
+}
 struct WavInfo {
-    chunks: Vec<WavChunk>,
+    chunks: Vec<FileChunk>,
 }
 
 fn parse_wav(file: &mut File) -> Result<WavInfo> {
@@ -72,20 +88,14 @@ fn parse_wav(file: &mut File) -> Result<WavInfo> {
 
     let mut buff4: [u8; 4] = [0; 4];
     file.read_exact(&mut buff4)?;
-    let fourcc = buff4;
+    let _fourcc = buff4;
 
     file.read_exact(&mut buff4)?;
-    let len = u32::from_le_bytes(buff4);
+    let _len = u32::from_le_bytes(buff4);
 
     file.read_exact(&mut buff4)?;
     let _format = String::from_utf8(buff4.to_vec())?;
     // TODO: abort parsing if not a "WAVE" file
-
-    wi.chunks.push(WavChunk {
-        offset: foffset,
-        chunk_id: fourcc,
-        chunk_size: len,
-    });
 
     foffset += 12;
 
@@ -101,10 +111,20 @@ fn parse_wav(file: &mut File) -> Result<WavInfo> {
             break;
         }
         let chunk_size = u32::from_le_bytes(buff4);
-        wi.chunks.push(WavChunk {
+
+        let chunk = match &chunk_id {
+            b"fmt " => {
+                let fmt = FmtChunk::read(file)?;
+                Chunk::Fmt(fmt)
+            }
+            _ => Chunk::UnParsed,
+        };
+
+        wi.chunks.push(FileChunk {
             offset: foffset,
             chunk_id,
             chunk_size,
+            chunk,
         });
 
         foffset += 8 + chunk_size;
@@ -131,6 +151,9 @@ enum Commands {
         // TODO: figure out the right way to make this an OS path safe string
         /// One or more paths to WAV files
         wav_path: Vec<OsString>,
+
+        #[arg(long, short)]
+        detailed: bool,
     },
 }
 
@@ -138,27 +161,18 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     match &args.command {
-        Commands::View { wav_path } => {
+        Commands::View { wav_path, detailed } => {
             // TODO: move command logic into a function
             for path in wav_path {
                 println!("{}", path.to_string_lossy());
                 let mut file = File::open(path)?;
                 let wavinfo = parse_wav(&mut file)?;
                 println!(
-                    "{:>12} {:4}{:>4} {:>10} description",
+                    "{:>12} {:4}{:>4} {:>10} summary",
                     "offset", "id", "", "size"
                 );
                 for chunk in wavinfo.chunks.iter() {
                     print!("{chunk}");
-                    if chunk.chunk_id == *b"fmt " {
-                        //println!("fmt chunk @ {0}", chunk.offset);
-                        file.seek(SeekFrom::Start(chunk.offset.into()))?;
-                        let fmt = FmtChunk::read(&mut file).unwrap();
-                        print!("{fmt}");
-                        // println!("{:?}", file.stream_position());
-                    } else {
-                        print!("...");
-                    };
                     println!();
                 }
                 println!();
