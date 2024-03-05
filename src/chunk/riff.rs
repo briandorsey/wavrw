8727b036be332b4f741c38ae43d3487b2ddeeeea
Brian Dorsey <brian@dorseys.org>
2024-03-04 20:32:35 -0800

    riff chunk: explicit test for large sizes
    Doublechecking to validate endian correctness.

--- src/chunk/riff.rs
+++ src/chunk/riff.rs
@@ -35,4 +35,22 @@ mod test {
             }
         );
     }
+
+    #[test]
+    fn parse_riff_large() {
+        // example large chunk size, double checking endian
+        let header = r#"52494646 0000FEFF 57415645 00000000"#;
+        let mut buff = hex_to_cursor(header);
+        println!("{header:?}");
+        let riff = Riff::read(&mut buff).expect("error parsing large data chunk");
+        print!("{:?}", riff);
+        assert_eq!(
+            riff,
+            Riff {
+                id: FourCC(*b"RIFF"),
+                size: 4294836224,
+                form_type: FourCC(*b"WAVE"),
+            }
+        );
+    }
 }
