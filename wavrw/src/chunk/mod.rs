5d160c58120e23bb3a844994bc1d0850b758d2d1
Brian Dorsey <brian@dorseys.org>
2024-04-27 13:57:29 -0700

    move module comments back into .rs source files

--- wavrw/src/chunk/mod.rs
+++ wavrw/src/chunk/mod.rs
@@ -1,4 +1,14 @@
-#![doc = include_str!("mod.md")]
+//! RIFF WAVE chunk parsers and structs.
+//!
+//! At a high level, each chunk module contains at least two structs, an inner
+//! data struct and a wrapper created with [`KnownChunk<T>`][crate::KnownChunk]
+//! type aliases. Ex: [`Fmt`] and [`FmtChunk`]. These type aliases are the primary
+//! interface to the chunks when reading from a file.
+//!
+//! For specifications and reference materials related to WAVE files, see the
+//! sibling project: [Wav Reference book](https://wavref.til.cafe/)
+//!
+//! TODO: write about architecture
 
 pub mod adtl;
 mod bext;
